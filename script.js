e674053c3e1cc290e10ec464a2d8267b3d67fd84
Sagnik <csagnik13@gmail.com>
2022-10-29 15:12:35 +0530

    Added basic hover effect and a button to change grid size and to reset hovered region

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-const container=document.querySelector('.container');
-for(let r=0;r<16;r++){
-    let row=document.createElement('div');
-    container.appendChild(row);
-    row.classList.add('gridRow');
-}
-let rows=document.querySelectorAll('.gridRow');
-for(let i=0;i<rows.length;i++){
-    for(let j=0;j<16;j++){
-        let newCell=document.createElement('div');
-        rows[j].appendChild(newCell);
-        newCell.classList.add('cell');
+let container=document.querySelector('.container');
+function makeGrid(gridSize){
+    for(let r=0;r<gridSize;r++){
+        let row=document.createElement('div');
+        container.appendChild(row);
+        row.classList.add('gridRow');
     }
+    let rows=document.querySelectorAll('.gridRow');
+    for(let i=0;i<rows.length;i++){
+        for(let j=0;j<gridSize;j++){
+            let cell=document.createElement('div');
+            rows[i].appendChild(cell);
+            cell.classList.add('cell');
+        }
+    }
+    let cells=document.querySelectorAll('.cell');
+    cells.forEach(cell=>{
+        cell.addEventListener('mouseover',()=>{
+            cell.classList.add('hover');
+        });
+    });
 }
+makeGrid(16);
+const btn1=document.querySelector('.button1');
+btn1.addEventListener('click',()=>{
+    let gridSize=parseInt(prompt('Enter new Grid Size'));
+    let rows=document.querySelectorAll('.gridRow');
+    rows.forEach(row=>{
+        container.removeChild(row);
+    });
+    makeGrid(gridSize);
+});
+const btn2=document.querySelector('.button2');
+btn2.addEventListener('click',()=>{
+    let cells=document.querySelectorAll('.cell');
+    cells.forEach(cell=>{
+        cell.classList.remove('hover');
+    });
+});
